960f0d624d0c95751e3c76e059e4cfabec2815d6
obondu <>
2011-01-13 16:59:10 +0000

    upload of resolution studies

---
+++ Zmumugamma/Selection/EResolution-studies/setrootenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# set good root env for roofit
+# Written by O. Bondu (January 2011)
+
+export ROOTSYS=/usr/local/root/root_v5.27.06
+export PATH=$ROOTSYS/bin:$PATH
+export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
+
+
