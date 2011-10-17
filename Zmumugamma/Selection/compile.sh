dc9d2a57fc6b81cbb4e56552569a4f79fe58515e
obondu <>
2011-10-17 15:00:28 +0000

    Making the file release-independant

--- Zmumugamma/Selection/compile.sh
+++ Zmumugamma/Selection/compile.sh
@@ -7,7 +7,8 @@ then
 fi
 exeFile=${1}
 
-g++ Selection_miniTree.C -L/afs/in2p3.fr/grid/toolkit/cms2/slc5_amd64_gcc434/lcg/root/5.27.06b-cms14/lib -lTMVA -lCore -lCint -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMLP -lTreePlayer -lMinuit -pthread -lm -ldl -rdynamic -pthread -m64 -I/afs/in2p3.fr/grid/toolkit/cms2/slc5_amd64_gcc434/lcg/root/5.27.06b-cms14/include -L`pwd` -lToto `root-config --libs --cflags` -o ${exeFile}
+
+g++ Selection_miniTree.C -L${ROOTSYS}lib -lTMVA -lCore -lCint -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMLP -lTreePlayer -lMinuit -pthread -lm -ldl -rdynamic -pthread -m64 -I${ROOTSYS}include -L`pwd` -lToto `root-config --libs --cflags` -o ${exeFile}
 
 #g++ Selection_miniTree.C -L`pwd` -lToto `root-config --libs --cflags` -o ${exeFile}
 
