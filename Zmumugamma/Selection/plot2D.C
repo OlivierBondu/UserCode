eddc38b8afd7f3d9b87a7c1a343222722243f4e8
obondu <>
2011-12-05 21:14:52 +0000

    update with variable binning for profiles

--- Zmumugamma/Selection/plot2D.C
+++ Zmumugamma/Selection/plot2D.C
@@ -1,3 +1,4 @@
+// ROOT HEADERS
 #include <TLorentzVector.h>
 #include <TVector3.h>
 #include <TTree.h>
@@ -15,23 +16,32 @@
 #include <TLeaf.h>
 #include <TChain.h>
 #include <TObject.h>
-#include <string.h>
-#include <algorithm>
 #include <TBranch.h>
 #include <TString.h>
 #include <TBits.h>
 #include <TMath.h>
 #include "TROOT.h"
+#include <TLatex.h>
+#include "TProof.h"
+
+// C++ HEADERS
+#include <string.h>
+#include <algorithm>
 #include <iostream>
 #include <iomanip>
 #include <sstream>
-#include <TLatex.h>
-#include "TProof.h"
+#include <vector>
+
+// DISPLAY HEADERS
 #include "CMSStyle.C"
+
+#pragma optimize 0
 
 using namespace std;
 
-int plot2D()
+
+//int plot2D()
+int main()
 {
 //	gROOT->ProcessLine(".x setTDRStyle.C");
 	CMSstyle();
@@ -51,7 +61,15 @@ int plot2D()
 
 	vector<string> x_variable;
 	vector<string> x_title;
-
+	vector<string> y_variable;
+	vector<string> y_title;
+	vector<int> x_nbins;
+//	typedef vector<double> vd;
+	vector< vector<double> > x_bins;
+//	vector<double*> x_bins;
+//	vector<double[]> x_bins;
+//return 3;
+/*
 	x_variable.push_back("MuonM_Phi");
 	x_title.push_back("#phi^{#mu^{-}}");
 	x_variable.push_back("MuonM_Eta");
@@ -93,21 +111,45 @@ int plot2D()
 	x_title.push_back("#eta^{#mu^{trail}}");
 	x_variable.push_back("MuonS_Pt");
 	x_title.push_back("E_{T}^{#mu^{trail}}");
-
+*/
 	x_variable.push_back("Photon_Et");
 	x_title.push_back("E_{T}^{#gamma}");
+	x_nbins.push_back(6);
+	double t[] = {10., 12., 15., 20., 25., 30., 100.};
+	vector<double> vt(t+0, t+7);
+	x_bins.push_back(vt);
+//	double t[] = {10., 12., 15., 20., 25., 30, 200.};
+//	x_bins.push_back(t);
+//);
+	y_variable.push_back("mmg_s * 100");
+  y_title.push_back("s (%)");
+	
+/*
 	x_variable.push_back("Photon_Eta");
 	x_title.push_back("#eta^{#gamma}");
+	y_variable.push_back("mmg_s * 100");
+  y_title.push_back("s (%)");
 	x_variable.push_back("Photon_Phi");
 	x_title.push_back("#phi^{#gamma}");
+	y_variable.push_back("mmg_s * 100");
+  y_title.push_back("s (%)");
+*/
 
-	
+cout << "x_nbins[0]= " << x_nbins[0] << endl;
+for( int isize= 0 ; isize <= x_nbins[0] ; isize++ ){
+	cout << "x_bins[0][" << isize << "]= " << x_bins[0][isize] << endl;
+}
+//double a[] = x_bins[0].toArray();
+//return 1;
 
 
 
 	vector<string> cat_cut;
 	vector<string> cat_title;
 
+	cat_cut.push_back("1");
+	cat_title.push_back("tight");
+/*
 	cat_cut.push_back("Photon_isEB");
 	cat_title.push_back("EB-tight");
 	cat_cut.push_back("Photon_isEB && Photon_r9 > .94");
@@ -120,23 +162,40 @@ int plot2D()
 	cat_title.push_back("EE-tight-highR9");
 	cat_cut.push_back("Photon_isEE && Photon_r9 < .95");
 	cat_title.push_back("EE-tight-lowR9");
-
+*/
 	string tight_cut = "isTightMMG && isMultipleCandidate == 0";
 
-	for(int idata = 0 ; idata < 2 ; idata++)
-//	for(int idata = 1 ; idata < 2 ; idata++)
+//	for(int idata = 0 ; idata < 2 ; idata++)
+	for(int idata = 1 ; idata < 2 ; idata++)
 	{
 		TChain *data = new TChain("miniTree");
 //		data->Add( idata == 0 ? "miniTree_v16_DATA_875pb-1.root" : "miniTree_v16_DYToMuMu.root");
-		data->Add( idata == 0 ? "miniTree_v17_Run2011A-ZMu-Jul05-Aug05-PromptSkim-v6.root" : "miniTree_v17_DYToMuMu.root");
+//		data->Add( idata == 0 ? "miniTree_v17_Run2011A-ZMu-Jul05-Aug05-PromptSkim-v6.root" : "miniTree_v17_DYToMuMu.root");
+			data->Add( idata == 0 ? "miniTree_v12_2011_40_80_partALL.root" : "miniTree_v10_DYToMuMu_S6_2011_40_80_partALL.root");
 		for(int icat = 0 ; icat < cat_cut.size() ; icat++)
 		{
 			for(int ivar = 0 ; ivar < x_variable.size() ; ivar++)
 			{
-				data->Draw(Form("mmg_s:%s", x_variable[ivar].c_str()), Form("(%s && %s )* weight_pileUp", tight_cut.c_str(), cat_cut[icat].c_str()), "COLZ");
-				TH2F* hist = (TH2F*)(gROOT->FindObject("htemp"));
-				hist->GetYaxis()->SetTitle("s");
-				hist->GetXaxis()->SetTitle(x_title[ivar].c_str());
+				TH2F* hist = new TH2F("hist", "hist", x_nbins[ivar], &x_bins[ivar][0], 500, -.5, .5);
+//				hist->SetBins(x_nbins[ivar], &x_bins[ivar][0]);
+//				data->Draw(Form("mmg_s:%s", x_variable[ivar].c_str()), Form("(%s && %s )* weight_pileUp", tight_cut.c_str(), cat_cut[icat].c_str()), "COLZ");
+				data->Draw(Form("(%s):(%s)>>hist", y_variable[ivar].c_str(), x_variable[ivar].c_str()), Form("(%s && %s )* weight_pileUp", tight_cut.c_str(), cat_cut[icat].c_str()), "goff");
+//				data->Draw(Form("(%s):(%s)>>hist", y_variable[ivar].c_str(), x_variable[ivar].c_str()), Form("(%s && %s )* weight_pileUp", tight_cut.c_str(), cat_cut[icat].c_str()), "COLZ");
+				//hist = (TH2F*)(gROOT->FindObject("htemp"));
+//				double *arr = &x_bins[0];
+//				std::copy(x_bins[ivar].begin(), x_bins[ivar].end(), arr);
+//cout << "oups" << endl;
+//				hist->SetBit(TH1::kCanRebin);
+//				hist->SetBins(x_nbins[ivar], arr);
+//				hist->SetBins(x_nbins[ivar], &x_bins[ivar][0]);
+//				hist->ProfileX()->Draw();
+//				TProfile* hist_pfx = (TProfile*)(gROOT->FindObject("hist_pfx"));
+				TProfile* hist_pfx = (TProfile*)(hist->ProfileX());
+				hist_pfx->SetMarkerStyle(25);
+				hist_pfx->SetMarkerSize(1);
+				hist_pfx->Draw();
+				hist_pfx->GetYaxis()->SetTitle(y_title[ivar].c_str());
+				hist_pfx->GetXaxis()->SetTitle(x_title[ivar].c_str());
 //				c1->Clear();
 //				TProfile *profile = (TProfile*) hist->ProfileX();
 //				profile->Draw();
@@ -156,7 +215,8 @@ int plot2D()
 				c1->Print(Form("png/%s_mmg_s_VS_%s_%s.png", idata == 0 ? "DATA" : "MC", x_variable[ivar].c_str(), cat_title[icat].c_str()));
 				c1->Print(Form("C/%s_mmg_s_VS_%s_%s.C", idata == 0 ? "DATA" : "MC", x_variable[ivar].c_str(), cat_title[icat].c_str()));
 				system(Form("convert eps/%s_mmg_s_VS_%s_%s.eps pdf/%s_mmg_s_VS_%s_%s.pdf", idata == 0 ? "DATA" : "MC", x_variable[ivar].c_str(), cat_title[icat].c_str(), idata == 0 ? "DATA" : "MC", x_variable[ivar].c_str(), cat_title[icat].c_str()));
-
+	
+				hist->Clear();
 				c1->Clear();
 //return 1;
 			} // END OF LOOP OVER VARIABLES
